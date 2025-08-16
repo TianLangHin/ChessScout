e59f2c475fe8bdfd47de72ec793f667a1a4257d0
TianLangHin <tianlanghin@gmail.com>
2025-08-16 20:16:29 +1000

    Initial Commit

---
+++ ChessScout/ChessScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessScoutApp.swift
+//  ChessScout
+//
+//  Created by Tian Lang Hin on 16/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChessScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
